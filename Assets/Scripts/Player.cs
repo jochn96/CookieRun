ac4981aa6e705eac46d2e8e945a6904a25c579ad
jochn96 <whtjdcks55@naver.com>
2025-05-08 20:51:40 +0900

    feat : 점프 기능 추가, 점프 슬라이딩 애니메이션 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,14 @@ public class Player : MonoBehaviour
     Animator animator;
     Rigidbody2D _rigidbody;
 
-    public float flapForce = 8f;
+    public float moveSpeed = 3f;
+    public float jumpForce = 8f;
     public bool isDead = false;
     float deathCooldown = 0f;
+    public float slideSpeed = 3f;
 
-    bool isFlap = false;
+    bool isJump = false;
+    bool isSlide = false;
 
     public bool godMode = false;
 
@@ -37,11 +40,43 @@ public class Player : MonoBehaviour
         {
 
         }
-        else
+        else 
         {
-            //if(Input.GetKeyDown(KeyCode.Space)) UI 만들고 추가 예정 
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isJump = true;
+            }
+
+            else if (Input.GetKeyDown(KeyCode.LeftShift))
+            {  
+                isSlide = true;
+            }    
+                       
         }
 
 
     }
+
+    private void FixedUpdate()
+    {
+        if(isDead) return;
+
+        Vector3 velocity = _rigidbody.velocity;
+        velocity.x = moveSpeed;
+
+        if (isJump)
+        {
+            velocity.y = jumpForce;
+            isJump = false;
+        }
+        else if (isSlide)
+        {
+            velocity.x = slideSpeed;
+            isSlide = false;
+        }
+
+        _rigidbody.velocity = velocity;
+
+    }
+
 }
